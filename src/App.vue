55269c5c9f4ff988157f3c1373621b43858ed01b
felipemuner <felipe.muner@gmail.com>
2020-07-30 18:38:36 +0700

    set up vuetify to try stepper vertical component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,60 @@
 <template>
-  <div id="app">
-    <div class="align-middle">
-      <BaseTimer />
-    </div>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import BaseTimer from "./components/BaseTimer.vue";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "App",
+  name: 'App',
+
   components: {
-    BaseTimer,
+    HelloWorld,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-body {
-  background: #058f8c;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.align-middle {
-  margin: auto;
-  width: 50%;
-}
-</style>
